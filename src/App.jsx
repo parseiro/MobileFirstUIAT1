0999fd4bd9ae05722a2aadc97b34bef72911f4c5
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-05-03 15:28:19 -0300

    Import react

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import {Head} from "./Head.jsx";
 import {Sidenav} from "./Sidenav.jsx";
 import {MainOverview} from "./MainOverview.jsx";
-
+import React from "react";
 
 export default function App() {
     return (
